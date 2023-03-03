7016c97131f825061f9e7eaaaf653cb6d1410c58
Olga Lobanova <arapova@list.ru>
2023-03-03 20:21:16 +0300

    RS

---
+++ Relationships.java
@@ -0,0 +1,13 @@
+package HWs.HW3.geotree;
+
+public enum Relationships {
+    PARENT,
+    CHILD,
+    SIBLING,
+    AUNT,
+    NIECE,
+    GRANDPARENT,
+    GRANDCHILD,
+    PARENT_IN_LAW,
+    DAUGHTER_IN_LOW
+}
